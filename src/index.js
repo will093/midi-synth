d956c4d146c3668df30750cb2c5daf4314c2e9d4
Will Taylor <willst.willst@gmail.com>
2018-09-24 22:10:31 +0100

    Unregister service worker before rendering app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import 'normalize.css/normalize.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
+
+// Unregister service worker.
+unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
